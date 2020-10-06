6295b70bc36854ea88f632605e9be022ac4731a9
murat1994 <gaday1037@gmail.com>
2020-10-06 23:35:01 +0300

    animal proje

---
+++ script.js
@@ -0,0 +1,13 @@
+let myAge = 26 ; // My Age....//
+let earlyYears = 2*10.5; //....//
+ earlyYears = earlyYears * 10.5
+ let laterYears = myAge - 2 ;
+ laterYears *= 4 ;
+ let myAgeinDogYears = laterYears + earlyYears ;
+ let myName = "Murat".toLowerCase();
+ console.log("My name is " + myName + " my age in dog years " + myAgeinDogYears);
+ 
+ 
+
+
+
